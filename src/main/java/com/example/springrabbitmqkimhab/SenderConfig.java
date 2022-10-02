e6b9c3f313ff98e539a303de25c23ec837e3ca41
kimhab <69948342+kimhab11@users.noreply.github.com>
2022-10-03 00:39:08 +0700

    Initial commit

---
+++ src/main/java/com/example/springrabbitmqkimhab/SenderConfig.java
@@ -0,0 +1,2 @@
+package com.example.springrabbitmqkimhab;public class SenderConfig {
+}
---
+++ src/main/java/com/example/springrabbitmqkimhab/SpringRabbitmqKimhabApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrabbitmqkimhab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitmqKimhabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRabbitmqKimhabApplication.class, args);
+    }
+
+}
