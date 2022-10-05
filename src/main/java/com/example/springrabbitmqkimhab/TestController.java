6dc8981ae6229bcd131b5b0d2b833598c76582c0
kimhab <69948342+kimhab11@users.noreply.github.com>
2022-10-05 15:16:08 +0700

    c

--- src/main/java/com/example/springrabbitmqkimhab/TestController.java
+++ src/main/java/com/example/springrabbitmqkimhab/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
-public class TesteController {
+public class TestController {
 
     @Autowired
     private QueueSender queueSender;
